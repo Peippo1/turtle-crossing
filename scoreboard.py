fa13f155b9c0198673d006660cc56df4b6ae4796
TimFinchJisc <tim.finch@jisc.ac.uk>
2022-03-26 18:07:24 +0000

    scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -10,4 +10,16 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.goto(-280, 250)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
         self.write(f"Level: {self.level}", align="left", fint=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="centre", font="FONT")
