449fdba03c7f5c0b7dfc7eaf763a27cf3cdd00e4
TimFinchJisc <tim.finch@jisc.ac.uk>
2022-03-26 17:40:31 +0000

    main.py

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ player = Player()
 car_manager = CarManager()
 
 screen.listen()
+
 screen.onkey(player.go_up, "Up")
 
+# Main game loop 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
     car_manager.create_cars()
+    
     car_manager.move_cars()
 
-    # Detect collision with car
+        # Detect collision with car
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
